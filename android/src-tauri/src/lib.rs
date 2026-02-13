c92b7754601bc618e1035fa8389d06fab096db58
ATCharpentier <atcharpentier@gmail.com>
2026-02-13 16:01:19 -0500

    Fix Android build: remove deep-link plugin (needs mobile setup)
    The deep-link plugin's build script expects mobile config as an array,
    not an object. Removing it entirely since deep links on Android require
    intent filters and domain verification that aren't configured yet.

--- android/src-tauri/src/lib.rs
+++ android/src-tauri/src/lib.rs
@@ -4,7 +4,6 @@ pub fn run() {
         .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_store::Builder::default().build())
         .plugin(tauri_plugin_shell::init())
-        .plugin(tauri_plugin_deep_link::init())
         .plugin(tauri_plugin_clipboard_manager::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
